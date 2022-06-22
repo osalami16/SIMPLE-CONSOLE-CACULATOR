fca2c2debaa5c871e5eafd76dee021dfad71e3c8
omolola taiwo <omololataiwo93@gmail.com>
2022-06-22 11:02:35 -0700

    first commit

---
+++ inform.js
@@ -0,0 +1,17 @@
+var opr = prompt("operation")
+var num1 = parseInt(prompt("first number 1 :"))
+var num2 =parseInt(prompt("second number 2 :"))
+
+if(opr == "+")
+{
+    alert(num1 + num2 )
+}else if(opr == "-")
+{
+    alert(num1 - num2)
+}else if(opr == "*")
+{
+    alert(num1 * num2)
+}else if(opr == "/")
+{
+    alert(num1 / num2)
+}
